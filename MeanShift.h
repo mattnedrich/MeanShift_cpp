b0ee80728363bcc159a9dd766aa24e4417551d35
Davide Faconti <davide.faconti@gmail.com>
2017-10-09 11:29:34 +0200

    less memory allocations

--- MeanShift.h
+++ MeanShift.h
@@ -10,14 +10,18 @@ struct Cluster {
 
 class MeanShift {
 public:
+    typedef std::vector<double> Point;
+
     MeanShift() { set_kernel(NULL); }
     MeanShift(double (*_kernel_func)(double,double)) { set_kernel(kernel_func); }
-    std::vector<std::vector<double> > meanshift(const std::vector<std::vector<double> > &, double);
-    std::vector<Cluster> cluster(const std::vector<std::vector<double> > &, double);
+    std::vector<Point> meanshift(const std::vector<Point> & points,
+                                                double kernel_bandwidth,
+                                                double EPSILON = 0.00001);
+    std::vector<Cluster> cluster(const std::vector<Point> &, double);
 
 private:
     double (*kernel_func)(double,double);
     void set_kernel(double (*_kernel_func)(double,double));
-    std::vector<double> shift_point(const std::vector<double> &, const std::vector<std::vector<double> > &, double);
-    std::vector<Cluster> cluster(const std::vector<std::vector<double> > &, const std::vector<std::vector<double> > &);
+    void shift_point(const Point&, const std::vector<Point> &, double, Point&);
+    std::vector<Cluster> cluster(const std::vector<Point> &, const std::vector<Point> &);
 };
